91e76d7eb464e239c5149d5d174b8ede90e18508
Lytton <xlordlytton@gmail.com>
2019-03-30 19:21:52 -0400

    nav bar completed, start aws

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -6,27 +6,40 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
     return (
         <nav className="main-header">
-            <ul className="primary-nav">
-                <li><p>Home</p></li>
-                <li><p>TV Shows</p></li>
-                <li><p>Movies</p></li>
-                <li><p>Recently Added</p></li>
-                <li><p>My List</p></li>
-            </ul>
+            <div className="prim-nav-logo">
+                <img className="landing-page-logo" src={window.logo1} alt="LandingPage Logo"/>
+                <ul className="primary-nav">
+                    <li><p>Home</p></li>
+                    <li><p>TV Shows</p></li>
+                    <li><p>Movies</p></li>
+                    <li><p>Recently Added</p></li>
+                    <li><p>My List</p></li>
+                </ul>
+            </div>
 
-            <ul className="secondary-nav">
-                <li><p>Search</p></li>
-                <li><p>KIDS</p></li>
-                <li><p>DVD</p></li>
-                <li><p>Notifications</p></li>
-                
+            <div className="sec-nav-dropdown">
+                <ul className="secondary-nav">
+                    <li><p>Search</p></li>
+                    <li><p>KIDS</p></li>
+                    <li><p>DVD</p></li>
+                    <li><p>Notifications</p></li>
+                    
+                </ul>
                 <div className="profile-dropdown">
-                    <li><p>User</p></li>
-                    <li><p>Account</p></li>
-                    <li><p>Help Center</p></li>
-                    <li><Link to="/" onClick={props.logout}><p>Log Out</p></Link></li>
+                    <img className="profile-icon" src={window.icon} alt="icon"/>
+                    <div className="dropdown-content">
+                        <div className="dropdown-section-1">
+                            <li><p>User</p></li>
+                        </div>
+                        <li className="profile-dropdown-divider"></li>
+                        <div className="dropdown-section-2">
+                            <li><p>Account</p></li>
+                            <li><p>Help Center</p></li>
+                            <li><Link to="/" onClick={props.logout}><p>Sign out of Netflix</p></Link></li>
+                        </div>
+                    </div>
                 </div>
-            </ul>
+            </div>
         </nav>
     )
 }
