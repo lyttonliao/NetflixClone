b7ac1cd6376f7421bbd459af5ef442179e242207
lyttonliao <lytton.liao@gmail.com>
2019-07-23 14:51:50 -0400

    Added traversal, suggestions, filtering, input methods, and searchbar

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -24,7 +24,7 @@ const search = props => {
         }
     }
 
-    // inserting into Trie
+    // inserting into Trie, sets word at index true
     const insert = (root, str, index) => {
         if (root === null) root = node()
 
@@ -70,7 +70,7 @@ const search = props => {
         }
     }
 
-    // searches for string
+    // searches for string, returns the map of indexes that is true
     const search = (root, str) => {
         if (root === null) return false
 
@@ -84,7 +84,94 @@ const search = props => {
         return temp.indexes
     }
 
-     
+    let suggested = []
+
+    // if root is a leaf (after going through a dfs traversal), push it into the suggested array
+    // traverses all children of the node
+    const traverse = (root, str) => {
+        if (root.isLeaf) {
+            suggested.push(str);
+            return
+        }
+
+        for (const [k, v] of root.map) {
+            traverse(v, str+String(k))
+        }
+    }
+
+    // add on to the words entered in search, adds onto suggested array
+    const suggestions = (root, str) => {
+        if (root === null) return false
+
+        let temp = root
+        for (let i = 0; i < str.length; i++) {
+            temp = temp.map.get(string[i])
+
+            if (!temp) return [];
+        }
+
+        if (!temp.isLeaf) {
+            for (const [k, v] of temp.map) {
+                traverse(v, str+String(k))
+            }
+        }
+    }
+
+    // for every movie, fill the Trie
+    for (let i = 0; i < data.length; i++) {
+        dfs(data[i], i)
+    }
+
+    //  searches for value based on target, fills filtered with the indexes of words that match the target
+    //  fills suggestion
+    const filteredData = (e, onEnter) => {
+        let searchedVal = search(Trie, e.target.value.toLowerCase())
+        let filtered = [];
+        suggested = [];
+        for (const [idx, _] of searchedVal) {
+            filtered.push(data[idx])
+        }
+
+        if (!onEnter) {
+            suggestions(Trie, e.target.value.toLowerCase(), suggested);
+        }
+
+        return {
+            filtered,
+            suggested
+        }
+    }
+
+    const inputSearch = (e) => {
+        if (e.target.value !== "") {
+            callbackify(filteredData(e, false))
+        } else {
+            callbackify({ filtered: [], suggested: [] })
+        }
+    }
+
+    const inputSearchOnEnter = (e) => {
+        if (e.which === 13) {
+            if (e.target.value !== "") {
+                callback(filteredData(e, false))
+            } else {
+                callback({ filtered: [], suggested: [] })
+            }
+        }
+    }
+
+    let SearchBar
+    if (onEnter === false) {
+        SearchBar = <input className="input-field" style={styles} placeholder="Search" onChange={ (e) => inputSearch(e) }/>
+    } else {
+        SearchBar = <input className="input-field" style={styles} placeholder="Search" onKeyPress={(e) => inputSearchOnEnter(e)} />
+    }
+
+    return (
+        <div>
+            {SearchBar}
+        </div>
+    )
 }
 
 export default Search
